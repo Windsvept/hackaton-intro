6240e2c9aecb3f9b5cbbef66e04555329686056c
Windsvept <junsubjjang@gmail.com>
2018-10-17 14:18:29 -0700

    first commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,11 +8,11 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <p>Here are some samples of what I've worked on</p>'
-    <a href="https://github.com/Windsvept">Github</a>
-    <a href="https://www.linkedin.com/in/junsubchoi/">LinkedIn</a>
-    <a href="https://github.com/yelpreactor/header-map">FEC (Front End Capstone) github Repo</a>
-    <a href="https://github.com/Hackazon-System-Design-Captstone/review-filter">SDC (System Design Capstone) github Repo</a>
-    <a href="https://somni-vindicta.herokuapp.com/">Full Stack 2D game</a>
+    <p href="https://github.com/Windsvept">Github</p>
+    <p href="https://www.linkedin.com/in/junsubchoi/">LinkedIn</p>
+    <p href="https://github.com/yelpreactor/header-map">FEC (Front End Capstone) github Repo</p>
+    <p href="https://github.com/Hackazon-System-Design-Captstone/review-filter">SDC (System Design Capstone) github Repo</p>
+    <p href="https://somni-vindicta.herokuapp.com/">Full Stack 2D game</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

09dc4d4e1be9500d2be8fd746d0eed0202c2faaa
Windsvept <junsubjjang@gmail.com>
2018-10-17 14:23:28 -0700

    first commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,11 +8,13 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <p>Here are some samples of what I've worked on</p>'
-    <p href="https://github.com/Windsvept">Github</p>
-    <p href="https://www.linkedin.com/in/junsubchoi/">LinkedIn</p>
-    <p href="https://github.com/yelpreactor/header-map">FEC (Front End Capstone) github Repo</p>
-    <p href="https://github.com/Hackazon-System-Design-Captstone/review-filter">SDC (System Design Capstone) github Repo</p>
-    <p href="https://somni-vindicta.herokuapp.com/">Full Stack 2D game</p>
+    <div>
+      <div><a href="https://github.com/Windsvept">Github</a></div>
+      <div><a href="https://www.linkedin.com/in/junsubchoi/">LinkedIn</a></div>
+      <div><a href="https://github.com/yelpreactor/header-map">FEC (Front End Capstone) github Repo</a></div>
+      <div><a href="https://github.com/Hackazon-System-Design-Captstone/review-filter">SDC (System Design Capstone) github Repo</a></div>
+      <div><a href="https://somni-vindicta.herokuapp.com/">Full Stack 2D game</a></div>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
